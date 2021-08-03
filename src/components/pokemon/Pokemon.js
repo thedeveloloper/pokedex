1642e0059885ae441308e12e8d028d87de751087
Chris Allen <tekkie1.618@gmail.com>
2021-08-03 09:58:01 -0400

    converting to functional component

--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -1,202 +1,200 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import { Loader, Container, Message, Image } from "semantic-ui-react";
 
 import TYPE_COLORS from "../../services/TypeColors";
 
-export default class Pokemon extends Component {
-  state = {
-    name: "",
-    url: "",
-    pokemonIndex: "",
-    imageUrl: "",
-    types: [],
-    description: "",
-    statTitleWidth: 3,
-    statBarWidth: 9,
-    stats: {
-      hp: "",
-      attack: "",
-      defense: "",
-      speed: "",
-      specialAttack: "",
-      specialDefense: "",
-    },
-    height: "",
-    weight: "",
-    eggGroups: "",
-    catchRate: "",
-    abilities: "",
-    genderRatioMale: "",
-    genderRatioFemale: "",
-    evs: "",
-    hatchSteps: "",
-    themeColor: "#EF5350",
-  };
+function Pokemon(props) {
+  const [name, setName] = useState("");
+  const [url, setUrl] = useState("");
+  const [pokemonIndex, setPokemonIndex] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [types, setTypes] = useState([]);
+  const [description, setDescription] = useState("");
+  const [hp, setHp] = useState("");
+  const [attack, setAttack] = useState("");
+  const [defense, setDefense] = useState("");
+  const [speed, setSpeed] = useState("");
+  const [specialAttack, setSpecialAttack] = useState("");
+  const [specialDefense, setSpecialDefense] = useState("");
+  const [stats, setStats] = useState({});
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+  const [eggGroups, setEggGroups] = useState("");
+  const [catchRate, setCatchRate] = useState("");
+  const [abilities, setAbilities] = useState("");
+  const [genderRatioMale, setGenderRatioMale] = useState("");
+  const [genderRatioFemale, setGenderRatioFemale] = useState("");
+  const [evs, setEvs] = useState("");
+  const [hatchSteps, setHatchSteps] = useState("");
+  const [themeColor, setThemeColor] = useState("#EF5350");
+  const [isLoading, setIsLoading] = useState(true);
+  const [imageLoading, setImageLoading] = useState(true);
+  const [tooManyRequests, setTooManyRequests] = useState(false);
 
-  async componentDidMount() {
-    const { pokemonIndex } = this.props.match.params;
+  const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}/`;
+  const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}/`;
+  setPokemonIndex(props.match.params);
 
-    // Urls for pokemon information
-    const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}/`;
-    const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}/`;
+  const pokemonRes = axios.get(pokemonUrl).then(setIsLoading(false));
 
-    // Get Pokemon Information
-    const pokemonRes = await axios.get(pokemonUrl);
-
-    const name = pokemonRes.data.name;
-    const imageUrl = pokemonRes.data.sprites.front_default;
-
-    let { hp, attack, defense, speed, specialAttack, specialDefense } = "";
+  if (isLoading === false) {
+    setName(pokemonRes.data.name);
+    setImageUrl(pokemonRes.data.sprites.front_default);
 
     pokemonRes.data.stats.map((stat) => {
       switch (stat.stat.name) {
         case "hp":
-          hp = stat["base_stat"];
+          setHp(stat["base_stat"]);
           break;
         case "attack":
-          attack = stat["base_stat"];
+          setAttack(stat["base_stat"]);
           break;
         case "defense":
-          defense = stat["base_stat"];
+          setDefense(stat["base_stat"]);
           break;
         case "speed":
-          speed = stat["base_stat"];
+          setSpeed(stat["base_stat"]);
           break;
         case "special-attack":
-          specialAttack = stat["base_stat"];
+          setSpecialAttack(stat["base_stat"]);
           break;
         case "special-defense":
-          specialDefense = stat["base_stat"];
+          setSpecialDefense(stat["base_stat"]);
           break;
         default:
           break;
       }
     });
 
-    // Convert Decimeters to Feet... The + 0.0001 * 100 ) / 100 is for rounding to two decimal places :)
-    const height =
-      Math.round((pokemonRes.data.height * 0.328084 + 0.00001) * 100) / 100;
+    setStats({
+      hp: hp,
+      attack: attack,
+      defense: defense,
+      speed: speed,
+      specialAttack: specialAttack,
+      specialDefense: specialDefense,
+    });
 
-    const weight =
-      Math.round((pokemonRes.data.weight * 0.220462 + 0.00001) * 100) / 100;
+    setHeight(
+      Math.round((pokemonRes.data.height * 0.328084 + 0.00001) * 100) / 100
+    );
 
-    const types = pokemonRes.data.types.map((type) => type.type.name);
+    setWeight(
+      Math.round((pokemonRes.data.weight * 0.220462 + 0.00001) * 100) / 100
+    );
 
-    const themeColor = `${TYPE_COLORS[types[types.length - 1]]}`;
+    setTypes(pokemonRes.data.types.map((type) => type.type.name));
 
-    const abilities = pokemonRes.data.abilities
-      .map((ability) => {
-        return ability.ability.name
-          .toLowerCase()
-          .split("-")
-          .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
-          .join(" ");
-      })
-      .join(", ");
+    setThemeColor(`${TYPE_COLORS[types[types.length - 1]]}`);
 
-    const evs = pokemonRes.data.stats
-      .filter((stat) => {
-        if (stat.effort > 0) {
-          return true;
-        }
-        return false;
-      })
-      .map((stat) => {
-        return `${stat.effort} ${stat.stat.name
-          .toLowerCase()
-          .split("-")
-          .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
-          .join(" ")}`;
-      })
-      .join(", ");
-
-    // Get Pokemon Description .... Is from a different end point uggh
-    await axios.get(pokemonSpeciesUrl).then((res) => {
-      let description = "";
-      res.data.flavor_text_entries.some((flavor) => {
-        if (flavor.language.name === "en") {
-          description = flavor.flavor_text;
-        }
-      });
-      const femaleRate = res.data["gender_rate"];
-      const genderRatioFemale = 12.5 * femaleRate;
-      const genderRatioMale = 12.5 * (8 - femaleRate);
-
-      const catchRate = Math.round((100 / 255) * res.data["capture_rate"]);
-
-      const eggGroups = res.data["egg_groups"]
-        .map((group) => {
-          return group.name
+    setAbilities(
+      pokemonRes.data.abilities
+        .map((ability) => {
+          return ability.ability.name
             .toLowerCase()
-            .split(" ")
+            .split("-")
             .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
             .join(" ");
         })
-        .join(", ");
-
-      const hatchSteps = 255 * (res.data["hatch_counter"] + 1);
-
-      this.setState({
-        description,
-        genderRatioFemale,
-        genderRatioMale,
-        catchRate,
-        eggGroups,
-        hatchSteps,
-      });
-    });
-
-    const url = "";
-
-    this.setState({
-      imageUrl,
-      pokemonIndex,
-      name,
-      url,
-      types,
-      stats: {
-        hp,
-        attack,
-        defense,
-        speed,
-        specialAttack,
-        specialDefense,
-      },
-      themeColor,
-      height,
-      weight,
-      abilities,
-      evs,
-    });
-  }
-
-  render() {
-    return (
-      <Container>
-        <Container color="#ccc" align="center">
-          <Image
-            src={this.state.imageLoading ? Loader : this.state.imageUrl}
-            size={"medium"}
-            onLoad={() => {
-              this.setState({ imageLoading: false });
-            }}
-            onError={() => {
-              this.setState({ tooManyRequests: true });
-            }}
-          />
-        </Container>
-        <Container>
-          <Message>Name: {this.state.name}</Message>
-          <Message>HP: {this.state.hp}</Message>
-          <Message>Attack: {this.state.attack}</Message>
-          <Message>Defense: {this.state.defense}</Message>
-          <Message>Speed: {this.state.speed}</Message>
-          <Message>Special Attack: {this.state.specialAttack}</Message>
-          <Message>Special Defense: {this.state.specialDefense}</Message>
-        </Container>
-      </Container>
+        .join(", ")
     );
+
+    setEvs(
+      pokemonRes.data.stats
+        .filter((stat) => {
+          if (stat.effort > 0) {
+            return true;
+          }
+          return false;
+        })
+        .map((stat) => {
+          return `${stat.effort} ${stat.stat.name
+            .toLowerCase()
+            .split("-")
+            .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" ")}`;
+        })
+        .join(", ")
+    );
+
+    axios.get(pokemonSpeciesUrl).then((res) => {
+      res.data.flavor_text_entries.some((flavor) => {
+        if (flavor.language.name === "en") {
+          setDescription(flavor.flavor_text);
+        }
+      });
+      const femaleRate = res.data["gender_rate"];
+      setGenderRatioFemale(12.5 * femaleRate);
+      setGenderRatioMale(12.5 * (8 - femaleRate));
+
+      setCatchRate(Math.round((100 / 255) * res.data["capture_rate"]));
+
+      setEggGroups(
+        res.data["egg_groups"]
+          .map((group) => {
+            return group.name
+              .toLowerCase()
+              .split(" ")
+              .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+              .join(" ");
+          })
+          .join(", ")
+      );
+
+      setHatchSteps(255 * (res.data["hatch_counter"] + 1));
+    });
   }
+
+  // const url = "";
+
+  // this.setState({
+  //   imageUrl,
+  //   pokemonIndex,
+  //   name,
+  //   url,
+  //   types,
+  //   stats: {
+  //     hp,
+  //     attack,
+  //     defense,
+  //     speed,
+  //     specialAttack,
+  //     specialDefense,
+  //   },
+  //   themeColor,
+  //   height,
+  //   weight,
+  //   abilities,
+  //   evs,
+  // });
+
+  return (
+    <Container>
+      <Container color="#ccc" align="center">
+        <Image
+          src={imageLoading ? Loader : imageUrl}
+          size={"medium"}
+          onLoad={() => {
+            setImageLoading(false);
+          }}
+          onError={() => {
+            setTooManyRequests(true);
+          }}
+        />
+      </Container>
+      <Container>
+        <Message>Name: {name}</Message>
+        <Message>HP: {hp}</Message>
+        <Message>Attack: {attack}</Message>
+        <Message>Defense: {defense}</Message>
+        <Message>Speed: {speed}</Message>
+        <Message>Special Attack: {specialAttack}</Message>
+        <Message>Special Defense: {specialDefense}</Message>
+      </Container>
+    </Container>
+  );
 }
+
+export default Pokemon;
