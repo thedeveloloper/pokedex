e19219a2976ad95b9ab0e6b8e5648472f77232e3
Chris Allen <tekkie1.618@gmail.com>
2021-08-03 08:59:18 -0400

    its learning

--- src/App.js
+++ src/App.js
@@ -3,24 +3,23 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import "semantic-ui-css/semantic.min.css";
 import { Container } from "semantic-ui-react";
+import "./App.css";
 
 import Dashboard from "./components/layout/Dashboard";
 import Pokemon from "./components/pokemon/Pokemon";
 
-import Background from "./res/pokeballs.jpg";
-
 function App() {
   return (
-    <Router>
-      <div style={{ background: `url(${Background})` }}>
+    <div className="background">
+      <Router>
         <Container fluid>
           <Switch>
             <Route exact path="/" component={Dashboard} />
             <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
           </Switch>
         </Container>
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
 
