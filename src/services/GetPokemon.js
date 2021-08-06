7f8fb0b9e57d8861f648d7a1ebaa6d58ad18d722
Chris Allen <tekkie1.618@gmail.com>
2021-08-06 11:37:56 -0400

    working on axios

--- src/services/GetPokemon.js
+++ src/services/GetPokemon.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+import { Image } from "semantic-ui-react";
+
 export const axiosGet = (url, query) => {
   return axios
     .get(url, {
@@ -18,11 +20,17 @@ export const axiosGet = (url, query) => {
     });
 };
 
-export const getPokemonList = (url) => {
+export const getPokemonList = (pageNumber, entriesPerPage = 20) => {
+  // const apiEndpoint = `https://pokeapi.co/api/v2/pokemon?offset=${pageNumber}&limit=20`;
+  const apiEndpoint = `https://pokeapi.co/api/v2/pokemon`;
   return axios
-    .get(url)
+    .get(apiEndpoint, {
+      params: {
+        offset: (pageNumber - 1) * 20,
+        limit: entriesPerPage,
+      },
+    })
     .then(function (res) {
-      console.log(res.data);
       return res.data;
     })
     .catch(function (error) {
@@ -30,6 +38,29 @@ export const getPokemonList = (url) => {
       return error;
     })
     .then(function () {
-      console.log("List aquired.");
+      console.log("Pokemon list aquired.");
     });
+};
+
+export const getPokemonInfo = (pokemonNumber) => {
+  const apiEndpoint = `https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`;
+  return axios
+    .get(apiEndpoint)
+    .then(function (res) {
+      return res.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+      return error;
+    })
+    .then(function () {
+      console.log("Pokemon info aquired.");
+    });
+};
+
+export const getPokemonImage = (pokemonNumber, shiny = false) => {
+  const imageUrl = shiny
+    ? `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/shiny/${pokemonNumber}.png?raw=true`
+    : `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonNumber}.png?raw=true`;
+  return <Image src={imageUrl} size={"medium"} />;
 };
