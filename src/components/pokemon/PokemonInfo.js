54345ac9337323b5d25843dcbb8f2a1c9539d6fa
Chris Allen <tekkie1.618@gmail.com>
2021-08-06 16:53:42 -0400

    working on info

--- src/components/pokemon/PokemonInfo.js
+++ src/components/pokemon/PokemonInfo.js
@@ -3,16 +3,25 @@ import React, { useState, useEffect } from "react";
 import PokemonImage from "../pokemon/PokemonImage";
 import { getPokemonInfo } from "../../services/GetPokemon";
 
-import { Modal, Button, Grid, Message, Header } from "semantic-ui-react";
+import {
+  Modal,
+  Button,
+  Grid,
+  Message,
+  Header,
+  Container,
+} from "semantic-ui-react";
 
 export default function PokemonInfo(props) {
   const [isOpen, setIsOpen] = useState(props.isOpen);
+  const [isLoading, setIsLoading] = useState(true);
   const [pokemonInfo, setPokemonInfo] = useState(null);
 
   useEffect(() => {
     setIsOpen(props.isOpen);
     const loadInfo = async () => {
       setPokemonInfo(await getPokemonInfo(props.pokemonNumber));
+      setIsLoading(false);
     };
     loadInfo();
   }, [props]);
@@ -24,17 +33,38 @@ export default function PokemonInfo(props) {
       onClose={() => setIsOpen(false)}
       onOpen={() => setIsOpen(true)}
     >
-      <Header textAlign="center">
-        {pokemonInfo.name
-          .split(" ")
-          .map((letter) => letter.charAt(0).toUpperCase() + letter.substring(1))
-          .join(" ")}
-      </Header>
-      <Grid textAlign="center">
-        <PokemonImage pokemonNumber={props.pokemonNumber} />
-        <PokemonImage pokemonNumber={props.pokemonNumber} showShiny={true} />
-      </Grid>
-      <Message></Message>
+      {isLoading ? null : (
+        <Container>
+          <Header textAlign="center">
+            {pokemonInfo.name
+              .split(" ")
+              .map(
+                (letter) => letter.charAt(0).toUpperCase() + letter.substring(1)
+              )
+              .join(" ")}
+          </Header>
+          <Grid textAlign="center">
+            <PokemonImage pokemonNumber={props.pokemonNumber} />
+            <PokemonImage
+              pokemonNumber={props.pokemonNumber}
+              showShiny={true}
+            />
+          </Grid>
+          <Message>
+            <Message.Header>Abilities</Message.Header>
+            {pokemonInfo.abilities
+              .map((a) => {
+                // console.log(a.ability);
+                return a.ability.name
+                  .toLowerCase()
+                  .split("-")
+                  .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+                  .join(" ");
+              })
+              .join(", ")}
+          </Message>
+        </Container>
+      )}
       <Button
         onClick={() => {
           setIsOpen(false);
