a134010a55b461e182ae58c23988ee232a53ec5a
Chris Allen <tekkie1.618@gmail.com>
2021-08-08 14:36:13 -0400

    worked on main page and centering

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { HashRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import "semantic-ui-css/semantic.min.css";
-import { Container } from "semantic-ui-react";
 import "./App.css";
 
 import Dashboard from "./components/layout/Dashboard";
@@ -11,14 +10,25 @@ import PokemonList from "./components/pokemon/PokemonList";
 function App() {
   return (
     <div className="background">
-      <Router>
-        <Container fluid>
-          <Switch>
-            <Route exact path="/" component={Dashboard} />
-            <Route exact path="/pokemon/list" component={PokemonList} />
-          </Switch>
-        </Container>
-      </Router>
+      <div align="center">
+        <div
+          style={{
+            align: "center",
+            width: "65%",
+            height: "100%",
+            minHeight: "100vh",
+            backgroundColor: "#DDD",
+            padding: "10px",
+          }}
+        >
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/pokemon" component={PokemonList} />
+            </Switch>
+          </Router>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -7,38 +7,32 @@ import PokemonList from "../pokemon/PokemonList";
 
 function Dashboard() {
   return (
-    <div align="center">
-      <div
-        style={{
-          align: "center",
-          height: "100%",
-          width: "65%",
-          backgroundColor: "#DDD",
-          padding: "10px",
-        }}
-      >
-        <Grid container textAlign="center">
-          <Grid.Row>
-            <Grid.Column>
-              <Button as={Link} to="/pokemon/list">
-                Pokemon List
-              </Button>
-            </Grid.Column>
-            <Grid.Column>
-              <Button>Item List</Button>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column>
-              <Button>Other List</Button>
-            </Grid.Column>
-            <Grid.Column>
-              <Button>Other List</Button>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </div>
-    </div>
+    <Grid columns={2}>
+      <Grid.Row>
+        <Grid.Column>
+          <Button circular color="blue" as={Link} to="/pokemon">
+            Pokemon List
+          </Button>
+        </Grid.Column>
+        <Grid.Column>
+          <Button circular color="blue" as={Link} to="">
+            Item List
+          </Button>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column>
+          <Button circular color="blue" as={Link} to="">
+            Other List
+          </Button>
+        </Grid.Column>
+        <Grid.Column>
+          <Button circular color="blue" as={Link} to="">
+            Other List
+          </Button>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   );
 }
 
